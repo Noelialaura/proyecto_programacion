d1ad940b8e945252198f393ef6a0045563db9e3a
Mile <mwp@uade.edu.ar>
2025-04-03 14:07:38 -0300

    Subo archivo con matriz de tarjetas rojas

---
+++ matriz tarjetas rojas.py
@@ -0,0 +1,8 @@
+matriz = [["Torres", "Ruiz", "Díaz", "Cruz", "Soto", "Vega", "Silva", "Ramos", "López", "Pérez", "Mora", "Cárdenas"],
+          [1, 2, 0, 4, 0, 1, 0, 0, 2, 0, 3, 5]]
+
+def main():
+    for fila in matriz:
+        print(fila)
+if __name__ == "__main__":
+    main()
