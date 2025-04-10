3394dc804e7f7eca5525dc2754c4c87f676c8b9f
Noelialaura <nlaura@uade.edu.ar>
2025-04-10 13:36:13 -0300

    subo info de los jugadores argentinos

---
+++ info argentina.py
@@ -0,0 +1,19 @@
+import random
+
+# Generar valores aleatorios para indicar la edad, cant de tarjetas rojas, asistencias y goles de los jugadores argentinos
+jugadores = [
+    {"nombre": "Lionel Messi", "edad": random.randint(30, 40), "tarjetas_rojas": random.randint(0, 5), "asistencias": random.randint(10, 30), "goles": random.randint(5, 50)},
+    {"nombre": "Lautaro Martinez", "edad": random.randint(20, 30), "tarjetas_rojas": random.randint(0, 5), "asistencias": random.randint(10, 30), "goles": random.randint(5, 50)},
+    {"nombre": "Nahuel Molina", "edad": random.randint(25, 35), "tarjetas_rojas": random.randint(0, 5), "asistencias": random.randint(10, 30), "goles": random.randint(5, 50)},
+    {"nombre": "Rodrigo De Paul", "edad": random.randint(30, 40), "tarjetas_rojas": random.randint(0, 5), "asistencias": random.randint(10, 30), "goles": random.randint(5, 50)},
+    {"nombre": "Emiliano Martinez", "edad": random.randint(20, 25), "tarjetas_rojas": random.randint(0, 5), "asistencias": random.randint(10, 30), "goles": random.randint(5, 50)},
+]
+
+# Mostrar los valores generados
+jugadores_Argentinos = [
+    f"Nombre: {jugador['nombre']}, Edad: {jugador['edad']}, Tarjetas Rojas: {jugador['tarjetas_rojas']}, Asistencias: {jugador['asistencias']}, Goles: {jugador['goles']}"
+    for jugador in jugadores
+]
+
+for jugador in jugadores_Argentinos:
+    print(jugador)
--- proyecto.py
+++ proyecto.py
@@ -4,15 +4,29 @@ def cantAsistencia():
     for i in range(0,5):
         lista.append(random.randint(1,20))
     return lista
+def cantGoles():
+    lista=[]
+    for i in range (0,5):
+        lista.append(random.randint(1,30))
+    return lista
+def tarjetasRojas():
+    lista=[]
+    for i in range (0,5):
+        lista.append(random.randint(1,10))
+    return lista
 def jugadores():
     nombresJugadores=["Carlos","antonio","luis","esteban","gustavo"]
     return nombresJugadores
+
 def main():
     cantidad = cantAsistencia()
     nombres = jugadores()
+    goles = cantGoles()
+    tarjetas=tarjetasRojas()
     for i in range(len(nombres)):
-        print (nombres[i],"  ", cantidad[i])
-  
+        print (nombres[i],"  ","cant asistencia: ", cantidad[i]," ","cant goles: ", goles[i]," ","tarjetas rojas: ",tarjetas[i])
+        
+
 main()
 
 
