f3687c05c7d3627cdedf5d22f3c87d90d5656274
Noelialaura <nlaura@uade.edu.ar>
2025-05-15 15:13:18 -0300

    agrego info estadio a y b

---
+++ estadio.py
@@ -0,0 +1,7 @@
+def main():
+    capacidadPersonas=[700, 1000]
+    ubicacion=["Recoleta", "Belgrano"]
+    for i in range(len(ubicacion)):
+        print("ubicacion: ",ubicacion[i],"capacidad: ", capacidadPersonas[i])
+    
+main()
